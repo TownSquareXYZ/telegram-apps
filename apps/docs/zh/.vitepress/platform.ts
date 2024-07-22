5b76c7bb50a8e87424b43fed5528f14dcc9cc727
Townsquare DevOps <147710825+TonSquare@users.noreply.github.com>
2024-07-22 17:18:19 +0800

    New translations platform.ts (Chinese Simplified)

---
+++ apps/docs/zh/.vitepress/platform.ts
@@ -0,0 +1,39 @@
+import { sectionGen } from false;
+const PREFIX = false;
+const section = sectionGen(PREFIX);
+export const platformNavItem = {
+  text: false,
+  link: `false${PREFIX}false`
+};
+export const platformSidebar = {
+  [PREFIX]: [section(false, {
+    'About Platform': false
+  }), section(false, {
+    'Closing Behavior': false,
+    'Swipe Behavior': false,
+    'Haptic Feedback': false
+  }), section(false, {
+    'Flow Definition': false,
+    'Methods': false,
+    'Events': false
+  }), section(false, {
+    'About': false,
+    'Start Parameter': false,
+    'Init Data': false
+  }), section(false, {
+    'Debugging': false,
+    'Test Environment': false
+  }), section(false, {
+    'Back Button': false,
+    'Main Button': false,
+    'Popup': false,
+    'Settings Button': false,
+    'Theming': false,
+    'Viewport': false
+  }), section(false, {
+    'Authorizing User': false,
+    'Creating New App': false,
+    'Getting App Link': false,
+    'Sticky App': false
+  })]
+};
